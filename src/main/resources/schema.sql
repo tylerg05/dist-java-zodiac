90617edc646148f591db556e078265f0e21c3c04
Stacy Read <sread@wctc.edu>
2020-09-14 00:17:35 -0500

    Added SQL files

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,9 @@
+create table Fortune (
+      fortune_id int primary key,
+      text varchar(1000) not null
+);
+
+create table Month (
+      num int primary key,
+      nm varchar(75) not null
+);
